b23c9e3a1400bedd1ea6cbe914d7aa72125798f5
beebapcay <69242061+beebapcay@users.noreply.github.com>
2024-08-07 22:46:57 +0700

    Implement gc:Slice

--- gc/gc.go
+++ gc/gc.go
@@ -1,28 +1,31 @@
 package gc
 
 import (
+	"fmt"
 	"generic-collections/guard"
 	"generic-collections/interfaces"
 )
 
 // Slice returns a new collection that contains a slice of the original collection.
 // The slice is defined by the index and length parameters.
-// If the length is positive, the slice will start at the index and end at the index + length. If index + length is greater than the collection length, the slice will end at the collection length.
-// If the length is negative, the slice will start at the index - length and end at the index.
+// If the index is greater than the length of the collection or the length is negative, a panic will occur.
+// If the length is greater than the length of the collection, the slice will try to get the remaining items and then wrap around to the beginning of the collection.
 func Slice[TType any](collection interfaces.IIndexableCollection[int, TType], index int, length int) interfaces.IIndexableCollection[int, TType] {
 	guard.EnsureIndexRange(index, collection.Count())
 
+	if length < 0 || length > collection.Count() {
+		panic(fmt.Sprintf("Length %d is out of range for list of length %d", length, collection.Count()))
+	}
+
 	out := (collection.Default().(any)).(interfaces.IIndexableCollection[int, TType])
-	if length == 0 {
-		return out
-	} else if length > 0 {
-		collection.ForEach(func(i int, item TType) {
-			if i >= index && i < index+length {
-				out.Add(item)
-			}
-		})
-	} else {
-		// TODO: Implement negative length
+
+	for i := 0; i < length; i++ {
+		slicePos := i + index
+		if i+index >= collection.Count() {
+			slicePos = i + index - collection.Count()
+		}
+
+		out.Add(collection.GetAt(slicePos))
 	}
 
 	return out
