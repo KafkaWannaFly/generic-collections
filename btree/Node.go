3c8485c019741d33e2902153b070c1b9dd1e7dc3
Kafka Wanna Fly <47100782+KafkaWannaFly@users.noreply.github.com>
2024-08-12 15:48:58 +0700

    Fix remove IComparer

--- btree/Node.go
+++ btree/Node.go
@@ -3,7 +3,6 @@ package btree
 import "github.com/KafkaWannaFly/generic-collections/interfaces"
 
 type Node[T any] struct {
-	interfaces.IComparer[Node[T]]
 	interfaces.IHashCoder
 
 	value T
@@ -11,5 +10,4 @@ type Node[T any] struct {
 	right *Node[T]
 }
 
-var _ interfaces.IComparer[Node[any]] = (*Node[any])(nil)
 var _ interfaces.IHashCoder = (*Node[any])(nil)
