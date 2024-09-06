194897c4116510277ff4d057ecf9322687a4993a
Duong Tan Phat <phat.duongtan@elca.vn>
2024-09-06 21:26:06 +0700

    BTree: Basic Functionality

--- btree/BTree.go
+++ btree/BTree.go
@@ -3,10 +3,143 @@ package btree
 import "github.com/KafkaWannaFly/generic-collections/interfaces"
 
 type BTree[T any] struct {
-	interfaces.ICollection[T]
-
 	root  *Node[T]
 	count int
 }
 
-var _ interfaces.ICollection[any] = (*BTree[any])(nil)
+var _ interfaces.ICollection[int] = (*BTree[int])(nil)
+
+func New[T any]() *BTree[T] {
+	return &BTree[T]{}
+}
+
+func From[T any](elements ...T) *BTree[T] {
+	tree := New[T]()
+	for _, element := range elements {
+		tree.Add(element)
+	}
+	return tree
+}
+
+func (receiver *BTree[T]) Add(item T) interfaces.ICollection[T] {
+	if receiver.root == nil {
+		receiver.root = NewLeafNode(item)
+	} else {
+		receiver.root.Add(item)
+	}
+	receiver.count++
+	return receiver
+}
+
+func (receiver *BTree[T]) AddAll(i interfaces.ICollection[T]) interfaces.ICollection[T] {
+	for _, item := range i.ToSlice() {
+		receiver.Add(item)
+	}
+	return receiver
+}
+
+func (receiver *BTree[T]) Count() int {
+	return receiver.count
+}
+
+func (receiver *BTree[T]) ForEach(appliedFunc func(int, T)) {
+	if receiver.root == nil {
+		return
+	}
+	receiver.root.ForEach(appliedFunc)
+}
+
+func (receiver *BTree[T]) Has(item T) bool {
+	if receiver.root == nil {
+		return false
+	}
+
+	return receiver.root.Has(item)
+}
+
+func (receiver *BTree[T]) HasAll(i interfaces.ICollection[T]) bool {
+	if receiver.root == nil {
+		return false
+	}
+
+	for _, item := range i.ToSlice() {
+		if !receiver.Has(item) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (receiver *BTree[T]) HasAny(i interfaces.ICollection[T]) bool {
+	if receiver.root == nil {
+		return false
+	}
+
+	for _, item := range i.ToSlice() {
+		if receiver.Has(item) {
+			return true
+		}
+	}
+	return false
+}
+
+func (receiver *BTree[T]) Remove(item T) interfaces.ICollection[T] {
+	if receiver.root == nil {
+		return receiver
+	}
+
+	receiver.root.Remove(item)
+	receiver.count--
+	return receiver
+}
+
+func (receiver *BTree[T]) Height() int {
+	if receiver.root == nil {
+		return 0
+	}
+	return receiver.root.Height()
+}
+
+func (receiver *BTree[T]) Clear() interfaces.ICollection[T] {
+	receiver.root = nil
+	receiver.count = 0
+	return receiver
+}
+
+func (receiver *BTree[T]) Filter(predicate func(T) bool) interfaces.ICollection[T] {
+	var filtered = New[T]()
+
+	receiver.ForEach(func(index int, item T) {
+		if predicate(item) {
+			filtered.Add(item)
+		}
+	})
+
+	return filtered
+}
+
+func (receiver *BTree[T]) ToSlice() []T {
+	var slice = make([]T, 0, receiver.count)
+	receiver.ForEach(func(index int, item T) {
+		slice = append(slice, item)
+	})
+	return slice
+}
+
+func (receiver *BTree[T]) IsEmpty() bool {
+	return receiver.count == 0
+}
+
+func (receiver *BTree[T]) Clone() interfaces.ICollection[T] {
+	var clone = New[T]()
+	receiver.ForEach(func(index int, item T) {
+		clone.Add(item)
+	})
+
+	return clone
+}
+
+func (receiver *BTree[T]) Default() interfaces.ICollection[T] {
+	return New[T]()
+}
--- btree/Node.go
+++ btree/Node.go
@@ -1,13 +1,178 @@
 package btree
 
-import "github.com/KafkaWannaFly/generic-collections/interfaces"
+import (
+	"github.com/KafkaWannaFly/generic-collections/interfaces"
+	"github.com/KafkaWannaFly/generic-collections/utils"
+	"math"
+)
 
 type Node[T any] struct {
-	interfaces.IHashCoder
-
 	value T
 	left  *Node[T]
 	right *Node[T]
 }
 
-var _ interfaces.IHashCoder = (*Node[any])(nil)
+var _ interfaces.IHashCoder = (*Node[interfaces.IHashCoder])(nil)
+
+func NewNode[T any](value T, left *Node[T], right *Node[T]) *Node[T] {
+	return &Node[T]{value: value, left: left, right: right}
+}
+
+func NewLeafNode[T any](value T) *Node[T] {
+	return NewNode(value, nil, nil)
+}
+
+// Clone creates a new Node with the same value as the receiver Node.
+// However, didn't copy the left and right fields.
+func (receiver *Node[T]) Clone() *Node[T] {
+	return NewNode(receiver.value, nil, nil)
+}
+
+func (receiver *Node[T]) HashCode() string {
+	return utils.HashCodeOf(receiver.value)
+}
+
+func (receiver *Node[T]) Equals(node *Node[T]) bool {
+	return receiver.HashCode() == node.HashCode()
+}
+
+func (receiver *Node[T]) LessThan(node *Node[T]) bool {
+	return receiver.HashCode() < node.HashCode()
+}
+
+func (receiver *Node[T]) ValueEquals(value T) bool {
+	return utils.HashCodeOf(receiver.value) == utils.HashCodeOf(value)
+}
+
+func (receiver *Node[T]) ValueLessThan(value T) bool {
+	return utils.HashCodeOf(receiver.value) < utils.HashCodeOf(value)
+}
+
+func (receiver *Node[T]) IsLeaf() bool {
+	return receiver.left == nil && receiver.right == nil
+}
+
+func (receiver *Node[T]) IsFull() bool {
+	return receiver.left != nil && receiver.right != nil
+}
+
+func (receiver *Node[T]) Add(value T) {
+	if receiver.ValueEquals(value) {
+		return
+	}
+
+	newNode := NewLeafNode(value)
+
+	if receiver.ValueLessThan(value) {
+		if receiver.left == nil {
+			receiver.left = newNode
+		} else {
+			receiver.left.Add(value)
+		}
+	} else {
+		if receiver.right == nil {
+			receiver.right = newNode
+		} else {
+			receiver.right.Add(value)
+		}
+	}
+}
+
+func (receiver *Node[T]) Remove(value T) *Node[T] {
+	if receiver == nil {
+		return nil
+	}
+
+	if receiver.ValueEquals(value) {
+		if receiver.IsLeaf() {
+			return nil
+		}
+
+		if receiver.left == nil {
+			return receiver.right
+		}
+
+		if receiver.right == nil {
+			return receiver.left
+		}
+
+		successor := receiver.right.Min()
+		receiver.value = successor.value
+		receiver.right = receiver.right.Remove(successor.value)
+		return receiver
+	}
+
+	if receiver.ValueLessThan(value) {
+		receiver.left = receiver.left.Remove(value)
+	} else {
+		receiver.right = receiver.right.Remove(value)
+	}
+
+	return receiver
+}
+
+func (receiver *Node[T]) Min() *Node[T] {
+	if receiver.left == nil {
+		return receiver
+	}
+
+	return receiver.left.Min()
+}
+
+func (receiver *Node[T]) Max() *Node[T] {
+	if receiver.right == nil {
+		return receiver
+	}
+
+	return receiver.right.Max()
+}
+
+func (receiver *Node[T]) Find(value T) *Node[T] {
+	if receiver == nil {
+		return nil
+	}
+
+	if receiver.ValueEquals(value) {
+		return receiver
+	}
+
+	if receiver.ValueLessThan(value) {
+		return receiver.left.Find(value)
+	}
+
+	return receiver.right.Find(value)
+}
+
+func (receiver *Node[T]) Height() int {
+	if receiver == nil {
+		return 0
+	}
+
+	return int(math.Max(float64(receiver.left.Height()), float64(receiver.right.Height()))) + 1
+}
+
+func (receiver *Node[T]) ForEach(appliedFunc func(int, T)) {
+	if receiver.left != nil {
+		receiver.left.ForEach(appliedFunc)
+	}
+	appliedFunc(-1, receiver.value)
+	if receiver.right != nil {
+		receiver.right.ForEach(appliedFunc)
+	}
+}
+
+func (receiver *Node[T]) Has(value T) bool {
+	if receiver == nil {
+		return false
+	}
+
+	if receiver.ValueEquals(value) {
+		return true
+	}
+
+	if receiver.ValueLessThan(value) {
+		return receiver.left.Has(value)
+	}
+
+	return receiver.right.Has(value)
+}
